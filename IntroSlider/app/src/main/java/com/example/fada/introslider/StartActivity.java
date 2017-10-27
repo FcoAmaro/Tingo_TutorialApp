a15411a74accd1c2c5b60b858a0638cb4402a07c
Unknown <cisc.amaro@gmail.com>
2017-10-27 13:30:38 -0300

    Version 1
    Intro Animada y scroll

---
+++ IntroSlider/app/src/main/java/com/example/fada/introslider/StartActivity.java
@@ -0,0 +1,41 @@
+package com.example.fada.introslider;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        imageView = (ImageView)findViewById(R.id.der);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.start_animation);
+        imageView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),TutorialActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
